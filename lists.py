8b7fc8e8d9fcc30472b4a751bf9fe76fbc96ce22
SimonBarilaro <162722001+SimonBarilaro@users.noreply.github.com>
2024-05-10 12:14:55 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,52 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if len(list_to_remove_elements) >=6:
+        del list_to_remove_elements[0]
+        del list_to_remove_elements[3]
+        del list_to_remove_elements[3]
+        return list_to_remove_elements
+    elif len(list_to_remove_elements)==5:
+        del list_to_remove_elements[0]
+        del list_to_remove_elements[3]
+        return list_to_remove_elements
+    elif len(list_to_remove_elements)==4:
+        del list_to_remove_elements[0]
+        return list_to_remove_elements
+    elif len(list_to_remove_elements)>=1:
+        del list_to_remove_elements[0]
+        return list_to_remove_elements
+    else:
+        return list_to_remove_elements
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    list_to_add_elements.insert(0,"Pink")
+    list_to_add_elements.append("Yellow")
+    return list_to_add_elements
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if len(list_to_check) == 0:
+        return True
+    else:
+        return False
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    len1=len(list_to_compare1)
+    len2=len(list_to_compare2)
+    if len1>=3 and len2>=3:
+        if list_to_compare1[2]==list_to_compare2[2]:
+            return True
+        else:
+            return False
+    else:
+        return False
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    lista1=list_of_lists_to_modify[0]
+    lista2=list_of_lists_to_modify[1]
+    lista3=list_of_lists_to_modify[2]
+    lista1=lista1[0:2]
+    lista2=lista2[1:4]
+    lista3=lista3[-2::1]
+    listafinal=lista1,lista2,lista3
+    listafinal=list(listafinal)
+    return listafinal
